0af17cf7d40a7c9f351c24b8f90d92fae571e2c5
leota <leotagit@gmail.com>
2023-06-17 17:25:52 +0200

    render from json input

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # blender-engine
 
+## Development
+`cd app && uvicorn main:app --reload`
+
+
+## Docker
 ```
 docker build -t blender-container .
 docker run -it --rm -p 8000:8000 -v /Users/leonardo/workspace/polygona/blender-server/projects:/app/projects blender-container
--- app/main.py
+++ app/main.py
@@ -1,21 +1,54 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from typing import List
+import os
+import json
+import hashlib
 import subprocess
+
+
+class Parameter(BaseModel):
+    name: str
+    value: str
+    type: str
+
+
+class Modifier(BaseModel):
+    name: str
+    parameters: List[Parameter]
+
+
+class BlenderInput(BaseModel):
+    object_name: str
+    modifiers: List[Modifier]
+
 
 app = FastAPI()
 
 
-@app.get("/render")
-def render():
+@app.post("/render")
+async def set_data(data: BlenderInput):
+    # convert data to dict and then to json
+    data_as_json = json.dumps(data.dict())
+
+    # create a hash of the json data
+    hash_object = hashlib.md5(data_as_json.encode())
+    hex_dig = hash_object.hexdigest()
+
+    # save json to file with hash as filename
+    json_file_path = os.path.abspath(f"/tmp/blender/{hex_dig}.json")
+    with open(json_file_path, "w") as file:
+        file.write(data_as_json)
+
     blender_command = [
         "blender",
-        "/app/projects/table.blend",
+        "/Users/leonardo/workspace/polygona/blender-engine/projects/table.blend",
         "--background",
         "--python",
         "blender_script.py",
-        "Procedural Table GN",
-        "Bevel",
-        "width",
-        "1.0",
+        json_file_path,
     ]
     subprocess.run(blender_command)
-    return {"message": "Rendering started"}
+
+    return {"message": "Data successfully received and stored", "filename": f"{hex_dig}.json"}
+
