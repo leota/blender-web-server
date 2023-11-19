a6ba2dd3e0792383870d247f24afe7c7720419b9
leota <leotagit@gmail.com>
2023-11-19 12:24:14 +0000

    app config

---
+++ app/config.py
@@ -0,0 +1,18 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    app_name: str = "Blender Engine API"
+    APP_ENV: str
+    ENVIRONMENT: str
+    SENTRY_DSN: str
+    DO_SPACES_REGION: str
+    DO_SPACES_ENDPOINT: str
+    DO_SPACES_BUCKET_NAME: str
+    DO_SPACES_KEY: str
+    DO_SPACES_SECRET: str
+    PROJECTS_FOLDER: str
+    OUTPUT_FOLDER: str
+
+    class Config:
+        env_file = ".env"
